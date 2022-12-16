6c6bb619e5405985f31bd08b96632ce111c6d628
wkoksik111 <102420085+wkoksik111@users.noreply.github.com>
2022-12-16 21:48:09 +0100

    Update bot.lua

--- szablon lua/bot.lua
+++ szablon lua/bot.lua
@@ -18,4 +18,32 @@ client:on('messageCreate', function(message)
 	end
 end)
 
+
+client:on('messageCreate', function(message)
+	if message.author.bot then return end
+    local args = message.content:split(" ")
+    local wiadomoscc = table.remove(args, 1)
+    if wiadomoscc == 'WOclear' then
+		if message.member:hasPermission('administrator') then
+			if os.time() > cooldown then
+				message:addReaction('✅')
+				message:delete()
+				local w = table.concat(args, " ")
+				local wd = message.channel:getMessages(w)
+				local wdp = (1)
+				message.channel:bulkDelete(wd)
+				local wiadomosc1 = message:reply("**🗑️ "..message.author.name.." usunął "..w.." wiadomości**")
+				cooldown = os.time() + 10
+				require('timer').sleep(1350)
+				wiadomosc1:delete()
+			else
+				message:reply("odczekaj jeszcze kilka sekund")
+			return end
+		else
+			message:reply("nie masz permisji aby wykonać tę czynność")
+        end
+    end
+end)
+
+
 client:run('Bot TOKEN')
